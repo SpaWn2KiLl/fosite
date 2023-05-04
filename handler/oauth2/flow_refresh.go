4025972767d6de87c368aeb594d06fe2d27c3e9e
Hilário Coelho <hilario.coelho@securityside.com>
2023-05-04 12:54:52 +0100

    retrieve original session

--- handler/oauth2/flow_refresh.go
+++ handler/oauth2/flow_refresh.go
@@ -6,6 +6,7 @@ package oauth2
 import (
 	"context"
 	"fmt"
+	"github.com/ory/fosite/handler/openid"
 	"github.com/pborman/uuid"
 	"strings"
 	"time"
@@ -78,7 +79,11 @@ func (c *RefreshTokenGrantHandler) HandleTokenEndpointRequest(ctx context.Contex
 		return errorsx.WithStack(fosite.ErrInvalidGrant.WithHint("The OAuth 2.0 Client ID from this request does not match the ID during the initial token issuance."))
 	}
 
+	fmt.Printf("Refresh Token Grant Handler session ID Token Clains (originalRequest): %+v\n", originalRequest.GetSession().(openid.Session).IDTokenClaims())
+	fmt.Printf("Refresh Token Grant Handler session ID Token Clains (request): %+v\n", request.GetSession().(openid.Session).IDTokenClaims())
 	request.SetSession(originalRequest.GetSession().Clone())
+	fmt.Printf("Refresh Token Grant Handler session ID Token Clains (originalRequest): %+v\n", originalRequest.GetSession().(openid.Session).IDTokenClaims())
+	fmt.Printf("Refresh Token Grant Handler session ID Token Clains (request): %+v\n", request.GetSession().(openid.Session).IDTokenClaims())
 	request.SetRequestedScopes(originalRequest.GetRequestedScopes())
 	request.SetRequestedAudience(originalRequest.GetRequestedAudience())
 
